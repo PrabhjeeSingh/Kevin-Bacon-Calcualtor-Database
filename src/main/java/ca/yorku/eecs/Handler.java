abbd4511cfedf6a62c7bdcb440cc966d8739c5cc
AggarwalAkshat <akshataggarwal2002@gmail.com>
2023-07-31 22:17:06 -0400

    first day
    Handler class has been created for the HttpExchange
    handleGet and HandlePut have been added with empty bodies

---
+++ src/main/java/ca/yorku/eecs/Handler.java
@@ -0,0 +1,95 @@
+package ca.yorku.eecs;
+import java.io.IOException;
+
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+public class Handler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange request) throws IOException {
+		// TODO Auto-generated method stub
+		try {
+            if (request.getRequestMethod().equals("GET")) {
+            	handleGet(request);
+                System.out.println("get");
+            }
+            else if(request.getRequestMethod().equals("PUT")) {
+            	handlePut(request);
+                System.out.println("put");
+            	}
+            else {
+            	sendString(request, "Unimplemented method\n", 501);
+            }
+            }
+		catch (Exception e) {
+        	e.printStackTrace();
+        	sendString(request, "Server error\n", 500);
+        }
+	}
+
+	private void handlePut(HttpExchange request) {
+		// TODO Auto-generated method stub
+		URI uri = request.getRequestURI();
+		String path=uri.getPath();
+		if(path=="/api/v1/addActor") {
+			//Complete
+		}
+		else if(path=="/api/v1/addMovie") {
+			//complete
+		}
+		else if(path=="/api/v1/addRelationship") {
+			//complete
+		}
+		String query = uri.getQuery();
+        System.out.println(query);
+        //Utils utils=new Utils();
+        Map<String, String> queryParam = Utils.splitQuery(query);
+        System.out.println(queryParam);
+        long first = Long.parseLong(queryParam.get("firstNumber"));
+        long second = Long.parseLong(queryParam.get("secondNumber"));
+		
+	}
+
+
+
+	private void handleGet(HttpExchange request)throws IOException {
+		// TODO Auto-generated method stub
+		URI uri = request.getRequestURI();
+		String path=uri.getPath();
+		if(path=="/api/v1/getActor") {
+			//Complete
+		}
+		else if(path=="/api/v1/getMovie") {
+			//complete
+		}
+		else if(path=="/api/v1/hasRelationship") {
+			//complete
+		}
+		else if(path=="/api/v1/computeBaconNumber") {
+			//complete
+		}
+		else if(path=="/api/v1/computeBaconPath") {
+			//complete
+		}
+		String query = uri.getQuery();
+        System.out.println(query);
+        //Utils utils=new Utils();
+        Map<String, String> queryParam = Utils.splitQuery(query);
+        System.out.println(queryParam);
+        long first = Long.parseLong(queryParam.get("firstNumber"));
+        long second = Long.parseLong(queryParam.get("secondNumber"));
+        
+        
+	}
+	private void sendString(HttpExchange request, String string, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
