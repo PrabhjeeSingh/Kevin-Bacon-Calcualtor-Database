3f3d488f6f905f8951272222511f89a104e79607
vrushank2001 <vrushankv1990@gmail.com>
2023-08-02 00:42:00 -0400

    hasMovie and getMovie methods added. Also, got to know from the Neo4j tutorial that we are using lowercase actor and movie, so changed in the class accordingly. Also, in the project handout it is specified that we have to name ids as actorId and movieId, so changed that too. And added some comments for methods.

--- src/main/java/ca/yorku/eecs/Neo4Jdatabase.java
+++ src/main/java/ca/yorku/eecs/Neo4Jdatabase.java
@@ -17,10 +17,14 @@ public class Neo4Jdatabase {
 		driver = GraphDatabase.driver(uriDb, AuthTokens.basic("neo4j","12345678"), config);
 	}
 	
-	public boolean hasActor(String actorID) {
+	/*
+	 * Returns true if the actor with given actorId is already present
+	 * in the database otherwise false.
+	 */
+	public boolean hasActor(String actorId) {
         try(Session session = driver.session()){
             Transaction transaction = session.beginTransaction();
-            String query = "MATCH (a: Actor) WHERE a.id = '" + actorID + "' RETURN a";
+            String query = "MATCH (a: actor) WHERE a.actorId = '" + actorId + "' RETURN a";
             StatementResult result = transaction.run(query);
             Boolean alreadyPresent = result.hasNext();
             transaction.success();
@@ -31,13 +35,62 @@ public class Neo4Jdatabase {
         }
     }
 	
-	public String getActor(String actorID) {
-		 if(hasActor(actorID) == false)
+	/*
+	 * Returns true if the movie with given movieId is already present
+	 * in the database otherwise false.
+	 */
+	public boolean hasMovie(String movieId) {
+        try(Session session = driver.session()){
+            Transaction transaction = session.beginTransaction();
+            String query = "MATCH (m: movie) WHERE m.movieId = '" + movieId + "' RETURN m";
+            StatementResult result = transaction.run(query);
+            Boolean alreadyPresent = result.hasNext();
+            transaction.success();
+            transaction.close();
+            session.close();
+            
+            return alreadyPresent;
+        }
+    }
+	/*
+	 * Returns the name of the actor with given actorId as a String.
+	 * If the actor is not present in the database then it returns
+	 * an empty string.
+	 */
+	public String getActor(String actorId) {
+		 if(hasActor(actorId) == false)
 			 return "";
 	       
 		 try(Session session = driver.session()){
 			 Transaction transaction = session.beginTransaction();
-			 String query = "MATCH (a: Actor) WHERE a.id = '" + actorID + "' RETURN a.Name";
+			 String query = "MATCH (a: actor) WHERE a.actorId = '" + actorId + "' RETURN a.name";
+			 StatementResult result = transaction.run(query);
+			 String name = result.next().values().get(0).asString();
+			 transaction.success();
+			 transaction.close();
+			 session.close();
+			 
+			 return name;
+		 }
+		 
+		 catch(Exception e) {
+			 e.printStackTrace();
+			 return "500 INTERNAL SERVER ERROR";
+		 }
+	}
+	
+	/*
+	 * Returns the name of the movie with given movieId as a String.
+	 * If the movie is not present in the database then it returns
+	 * an empty string.
+	 */
+	public String getMovie(String movieId) {
+		 if(hasActor(movieId) == false)
+			 return "";
+	       
+		 try(Session session = driver.session()){
+			 Transaction transaction = session.beginTransaction();
+			 String query = "MATCH (m: movie) WHERE m.movieId = '" + movieId + "' RETURN m.name";
 			 StatementResult result = transaction.run(query);
 			 String name = result.next().values().get(0).asString();
 			 transaction.success();
