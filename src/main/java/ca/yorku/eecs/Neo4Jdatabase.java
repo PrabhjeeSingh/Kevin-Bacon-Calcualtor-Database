2c7d4074e0dc4156e853d8654324130089ad7b82
vrushank2001 <vrushankv1990@gmail.com>
2023-08-12 19:59:46 -0400

    actorId --> id, movieId --> id, value --> year and some minor changes accordingly

--- src/main/java/ca/yorku/eecs/Neo4Jdatabase.java
+++ src/main/java/ca/yorku/eecs/Neo4Jdatabase.java
@@ -20,13 +20,13 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Returns true if the actor with given actorId is already present
+	 * Returns true if the actor with given id is already present
 	 * in the database otherwise false.
 	 */
-	public boolean hasActor(String actorId) {
+	public boolean hasActor(String id) {
         try(Session session = driver.session()){
             Transaction transaction = session.beginTransaction();
-            String query = "MATCH (a: actor) WHERE a.actorId = '" + actorId + "' RETURN a;";
+            String query = "MATCH (a: actor) WHERE a.id = '" + id + "' RETURN a;";
             StatementResult result = transaction.run(query);
             
             boolean actorAlreadyPresent = result.hasNext();
@@ -40,13 +40,13 @@ public class Neo4Jdatabase {
     }
 	
 	/*
-	 * Returns true if the movie with given movieId is already present
+	 * Returns true if the movie with given id is already present
 	 * in the database otherwise false.
 	 */
-	public boolean hasMovie(String movieId) {
+	public boolean hasMovie(String id) {
         try(Session session = driver.session()){
             Transaction transaction = session.beginTransaction();
-            String query = "MATCH (m: movie) WHERE m.movieId = '" + movieId + "' RETURN m;";
+            String query = "MATCH (m: movie) WHERE m.id = '" + id + "' RETURN m;";
             StatementResult result = transaction.run(query);
             //System.out.println("result  = " + result);
             boolean movieAlreadyPresent = result.hasNext();
@@ -61,17 +61,17 @@ public class Neo4Jdatabase {
 		
     }
 	/*
-	 * Returns the name of the actor with given actorId as a String.
+	 * Returns the name of the actor with given id as a String.
 	 * If the actor is not present in the database then it returns
 	 * an empty string.
 	 */
-	public String getActorName(String actorId) {
-		 if(hasActor(actorId) == false)
+	public String getActorName(String id) {
+		 if(hasActor(id) == false)
 			 return "";
 	       
 		 try(Session session = driver.session()){
 			 Transaction transaction = session.beginTransaction();
-			 String query = "MATCH (a: actor) WHERE a.actorId = '" + actorId + "' RETURN a.name AS name;";
+			 String query = "MATCH (a: actor) WHERE a.id = '" + id + "' RETURN a.name AS name;";
 			 StatementResult result = transaction.run(query);
 			 
 			 String name = result.next().get("name").asString();
@@ -90,18 +90,18 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Returns the name of the movie with given movieId as a String.
+	 * Returns the name of the movie with given id as a String.
 	 * If the movie is not present in the database then it returns
 	 * an empty string.
 	 */
-	public String getMovieName(String movieId) {
-		 if(hasMovie(movieId) == false) {
+	public String getMovieName(String id) {
+		 if(hasMovie(id) == false) {
 		 	//System.out.println("movie not found");
 			 return "";
 		 }
 		 try(Session session = driver.session()){
 			 Transaction transaction = session.beginTransaction();
-			 String query = "MATCH (m: movie) WHERE m.movieId = '" + movieId + "' RETURN m.name AS name;";
+			 String query = "MATCH (m: movie) WHERE m.id = '" + id + "' RETURN m.name AS name;";
 			 StatementResult result = transaction.run(query);
 			 
 			 String name = result.next().get("name").asString();
@@ -120,18 +120,18 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Returns the list of movies of an actor with given actorId has acted in.
+	 * Returns the list of movies of an actor with given id has acted in.
 	 */
-	public List<String> getMoviesOfActor(String actorId){
+	public List<String> getMoviesOfActor(String id){
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query =  "MATCH (a: actor {actorId: '" + actorId + "'})-[:ACTED_IN]->(fof) RETURN DISTINCT fof.movieId AS movieId;";
+			String query =  "MATCH (a: actor {id: '" + id + "'})-[:ACTED_IN]->(fof) RETURN DISTINCT fof.id AS id;";
 			StatementResult result = transaction.run(query);
 			
 			List<String> listOfMovies = new ArrayList<>();
 			
 			while(result.hasNext()) {
-				listOfMovies.add(result.next().get("movieId").asString());
+				listOfMovies.add(result.next().get("id").asString());
 			}
 			
 			transaction.success();
@@ -143,18 +143,18 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Returns the list of movies of an actor with given actorId has acted in.
+	 * Returns the list of movies of an actor with given id has acted in.
 	 */
-	public List<String> getActorsOfMovie(String movieId){
+	public List<String> getActorsOfMovie(String id){
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query =  "MATCH (a: actor)-[:ACTED_IN]->(m: movie {movieId : '" + movieId + "'}) RETURN DISTINCT a.actorId AS actorId;";
+			String query =  "MATCH (a: actor)-[:ACTED_IN]->(m: movie {id : '" + id + "'}) RETURN DISTINCT a.id AS id;";
 			StatementResult result = transaction.run(query);
 			
 			List<String> listOfActors = new ArrayList<>();
 			
 			while(result.hasNext()) {
-				listOfActors.add(result.next().get("actorId").asString());
+				listOfActors.add(result.next().get("id").asString());
 			}
 			
 			transaction.success();
@@ -175,7 +175,7 @@ public class Neo4Jdatabase {
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query =  "MATCH (a: actor {actorId: '"+ actorId + "'})-[:ACTED_IN]->(m: movie {movieId: '" + movieId + "'}) RETURN EXISTS((a)-[:ACTED_IN]->(m));";
+			String query =  "MATCH (a: actor {id: '"+ actorId + "'})-[:ACTED_IN]->(m: movie {id: '" + movieId + "'}) RETURN EXISTS((a)-[:ACTED_IN]->(m));";
 			StatementResult result = transaction.run(query);
 			
 			boolean isRelationshipPresent = result.next().get(0).asBoolean();
@@ -196,16 +196,16 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Adds the actor with given name and actorId to the database.
+	 * Adds the actor with given name and id to the database.
 	 */
-	public String addActor(String name, String actorId) {
+	public String addActor(String name, String id) {
 		
-		if(hasActor(actorId))
+		if(hasActor(id))
 			return "400 BAD REQUEST";
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "CREATE (a: actor {name: '" + name + "', actorId: '" + actorId + "'});";
+			String query = "CREATE (a: actor {name: '" + name + "', id: '" + id + "'});";
 			//StatementResult result = transaction.run(query);
 			transaction.run(query);
 			
@@ -222,16 +222,16 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Adds the movie with given name and movieId to the database.
+	 * Adds the movie with given name and id to the database.
 	 */
-	public String addMovie(String name, String movieId) {
+	public String addMovie(String name, String id) {
 		
-		if(hasMovie(movieId))
+		if(hasMovie(id))
 			return "400 BAD REQUEST";
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "CREATE (m: movie {name: '" + name + "', movieId: '" + movieId + "'});";
+			String query = "CREATE (m: movie {name: '" + name + "', id: '" + id + "'});";
 			//StatementResult result = transaction.run(query);
 			transaction.run(query);
 			
@@ -262,7 +262,7 @@ public class Neo4Jdatabase {
 			
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "MATCH (a: actor), (m: movie) WHERE a.actorId = '" + actorId + "' AND m.movieId = '" + movieId + "' CREATE (a)-[r:ACTED_IN]->(m);";
+			String query = "MATCH (a: actor), (m: movie) WHERE a.id = '" + actorId + "' AND m.id = '" + movieId + "' CREATE (a)-[r:ACTED_IN]->(m);";
 			//StatementResult result = transaction.run(query);
 			transaction.run(query);
 			
@@ -279,19 +279,19 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Computes the bacon number of the actor with given actorId
-	 * with respect to Kevin Bacon with actorId "nm0000102".
+	 * Computes the bacon number of the actor with given id
+	 * with respect to Kevin Bacon with id "nm0000102".
 	 */
-	public String computeBaconNumber(String actorId) {
-		if(!hasActor(actorId))
+	public String computeBaconNumber(String id) {
+		if(!hasActor(id))
 			return "404 NOT FOUND";
 		
-		if(actorId.equals("nm0000102"))
+		if(id.equals("nm0000102"))
 			return "0";
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "MATCH path = shortestPath((kb: actor {actorId: 'nm0000102'})-[*]-(a: actor {actorId: '" + actorId +"'})) RETURN length(path)/2 AS baconNumber;";
+			String query = "MATCH path = shortestPath((kb: actor {id: 'nm0000102'})-[*]-(a: actor {id: '" + id +"'})) RETURN length(path)/2 AS baconNumber;";
 			StatementResult result = transaction.run(query);
 			
 			int baconNumber = result.next().get("baconNumber").asInt();
@@ -308,19 +308,19 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Computes the bacon path of the actor with given actorId
-	 * with respect to Kevin Bacon with actorId "nm0000102".
+	 * Computes the bacon path of the actor with given id
+	 * with respect to Kevin Bacon with id "nm0000102".
 	 */
-	public List<String> computeBaconPath(String actorId){
-		if(!hasActor(actorId))
+	public List<String> computeBaconPath(String id){
+		if(!hasActor(id))
 			return new ArrayList<>(Arrays.asList(new String[]{"404 NOT FOUND"}));
 		
-		if(actorId.equals("nm0000102"))
+		if(id.equals("nm0000102"))
 			return new ArrayList<>(Arrays.asList(new String[] {"nm0000102"}));
 		 
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "MATCH path = shortestPath((kb: actor {actorId: 'nm0000102'})-[*]-(a: actor {actorId: '" + actorId +"'})) RETURN nodes(path) AS path";
+			String query = "MATCH path = shortestPath((kb: actor {id: 'nm0000102'})-[*]-(a: actor {id: '" + id +"'})) RETURN nodes(path) AS path";
 			StatementResult result = transaction.run(query);
 			
 			//List<Object> pathObject = result.next().get("path").asList();
@@ -329,14 +329,15 @@ public class Neo4Jdatabase {
 			List<Node> pathNode = result.next().get("path").asList(Value::asNode);
 			
 			for(Node data_entity : pathNode) {
-				//Map<String, Object> properties = ((Value) value).asMap();
+				//Map<String, Object> properties = ((Value) year).asMap();
 				
-				if(data_entity.hasLabel("actor")) {
-					baconPath.add(data_entity.get("actorId").asString());
-				}
-				else if(data_entity.hasLabel("movie")) {
-					baconPath.add(data_entity.get("movieId").asString());
-				}
+					baconPath.add(data_entity.get("id").asString());
+					/*if(data_entity.hasLabel("actor")) {
+						baconPath.add(data_entity.get("actorId").asString());
+					}
+					else if(data_entity.hasLabel("movie")) {
+						baconPath.add(data_entity.get("movieId").asString());
+					}*/
 			}
 			
 			transaction.success();
@@ -354,13 +355,13 @@ public class Neo4Jdatabase {
 	//METHODS FOR NEW FEATURE STARTS FROM HERE.
 	
 	/*
-	 * Returns true if the year node with given value (year) is already present
+	 * Returns true if the year node with given year is already present
 	 * in the database otherwise false.
 	 */
-	public boolean hasYear(String value) {
+	public boolean hasYear(String year) {
         try(Session session = driver.session()){
             Transaction transaction = session.beginTransaction();
-            String query = "MATCH (y: year) WHERE y.value = '" + value + "' RETURN y;";
+            String query = "MATCH (y: year) WHERE y.year = '" + year + "' RETURN y;";
             StatementResult result = transaction.run(query);
             
             boolean yearAlreadyPresent = result.hasNext();
@@ -374,16 +375,16 @@ public class Neo4Jdatabase {
     }
 	
 	/*
-	 * Checks whether the relationship between movie with given movieId
-	 * and year node with given value (year) exists or not.
+	 * Checks whether the relationship between movie with given id
+	 * and year node with given year exists or not.
 	 */
-	public String hasRelationshipBtwnMovieYear(String movieId, String value) {
-		if(!hasMovie(movieId) || !hasYear(value))
+	public String hasRelationshipBtwnMovieYear(String id, String year) {
+		if(!hasMovie(id) || !hasYear(year))
 			return "404 NOT FOUND";
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query =  "MATCH (m: movie {movieId: '"+ movieId + "'})-[:RELEASED_IN]->(y: year {value: '" + value + "'}) RETURN EXISTS((m)-[:RELEASED_IN]->(y));";
+			String query =  "MATCH (m: movie {id: '"+ id + "'})-[:RELEASED_IN]->(y: year {year: '" + year + "'}) RETURN EXISTS((m)-[:RELEASED_IN]->(y));";
 			StatementResult result = transaction.run(query);
 			
 			boolean isRelationshipPresent = result.next().get(0).asBoolean();
@@ -404,16 +405,16 @@ public class Neo4Jdatabase {
 	}
 	
 	/*
-	 * Adds the year node with given value (year) to the database.
+	 * Adds the year node with given year to the database.
 	 */
-	public String addYear(String value) {
+	public String addYear(String year) {
 		
-		if(hasYear(value))
+		if(hasYear(year))
 			return "400 BAD REQUEST";
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "CREATE (y: year {value: '" + value + "'});";
+			String query = "CREATE (y: year {year: '" + year + "'});";
 			//StatementResult result = transaction.run(query);
 			transaction.run(query);
 			
@@ -434,17 +435,17 @@ public class Neo4Jdatabase {
 	 * movie RELEASED_IN year
 	 * to the database.
 	 */
-	public String addRelationshipBtwnMovieYear(String movieId, String value) {
+	public String addRelationshipBtwnMovieYear(String id, String year) {
 		
-		if(!hasMovie(movieId) || !hasYear(value))
+		if(!hasMovie(id) || !hasYear(year))
 			return "404 NOT FOUND";
 		
-		if(hasRelationshipBtwnMovieYear(movieId, value).equals("true"))
+		if(hasRelationshipBtwnMovieYear(id, year).equals("true"))
 			return "400 BAD REQUEST";
 			
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query = "MATCH (m: movie), (y: year) WHERE m.movieId = '" + movieId + "' AND y.value = '" + value + "' CREATE (m)-[rmy:RELEASED_IN]->(y);";
+			String query = "MATCH (m: movie), (y: year) WHERE m.id = '" + id + "' AND y.year = '" + year + "' CREATE (m)-[rmy:RELEASED_IN]->(y);";
 			//StatementResult result = transaction.run(query);
 			transaction.run(query);
 			
@@ -463,19 +464,19 @@ public class Neo4Jdatabase {
 	/*
 	 * Returns the list of movies released on a specific year.
 	 */
-	public List<String> getMoviesOfYear(String value){
-		if(!hasYear(value))
+	public List<String> getMoviesOfYear(String year){
+		if(!hasYear(year))
 			return new ArrayList<>(Arrays.asList(new String[] {"404 NOT FOUND"}));
 		
 		try(Session session = driver.session()){
 			Transaction transaction = session.beginTransaction();
-			String query =  "MATCH (m: movie)-[:RELEASED_IN]->(y: year {value : '" + value + "'}) RETURN DISTINCT m.movieId AS movieId;";
+			String query =  "MATCH (m: movie)-[:RELEASED_IN]->(y: year {year : '" + year + "'}) RETURN DISTINCT m.id AS id;";
 			StatementResult result = transaction.run(query);
 			
 			List<String> listOfMovies = new ArrayList<>();
 			
 			while(result.hasNext()) {
-				listOfMovies.add(result.next().get("movieId").asString());
+				listOfMovies.add(result.next().get("id").asString());
 			}
 			
 			transaction.success();
